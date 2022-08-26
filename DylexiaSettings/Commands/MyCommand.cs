d00051bb87492a8c1f67fad125d0414cce4c7d40
Sandy Grawe <sgrawe@aol.co>
2022-08-26 15:58:10 +0200

    Init VSIX Project w/Command

---
+++ DylexiaSettings/Commands/MyCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace DylexiaSettings
+{
+    [Command(PackageIds.MyCommand)]
+    internal sealed class MyCommand : BaseCommand<MyCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            await VS.MessageBox.ShowWarningAsync("DylexiaSettings", "Button clicked");
+        }
+    }
+}
